bc5fb51bf5096898fa41a53e3723ebe0e5f25cf0
cdmccune <curtmccune@gmail.com>
2022-04-28 14:43:02 -0700

    added endpoint

--- server/index.js
+++ server/index.js
@@ -5,19 +5,29 @@ const app = express()
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('file served')
 })
+
 
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
-  accessToken: '42d1d806099e48d7a7d00d2980a2f080',
+    accessToken: '42d1d806099e48d7a7d00d2980a2f080',
   captureUncaught: true,
   captureUnhandledRejections: true,
 })
 
+app.get('/test', () => {try{
+    banana()
+} catch(error){rollbar.error(error)}})
+
+
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-app.get('/bleh', ()=> {getHouse()})
+// app.get("/error", (req, res) => {
+//     rollbar.error('This is an error')
+//     res.status(400).send('this is an error')
+// })
 
 app.use(express.json())
 
