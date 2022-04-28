b06a6186bba54e39f38f0d0e190b58d47cdee9c6
cdmccune <curtmccune@gmail.com>
2022-04-28 14:06:16 -0700

    initial commit

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+app.use(express.json())
+
+const port = process.env.PORT || 4545
+
+app.listen(port, () => {console.log(`listening on port ${port}`)})
